eaa8e7f9118e3c3ce7d5d949c17dd9e47e6b9f0c
Tobias <github@friman.name>
2024-02-27 22:11:59 +0100

    Added a simple computer opponent

--- lab04_TF/board.py
+++ lab04_TF/board.py
@@ -28,11 +28,16 @@ def place(board,marker,row,column):
     board[row][column] = marker
     return True
 
+def remove(board,row,column):
+    if is_empty(board,row,column): return False
+    board[row][column] = '-'
+    return True
+
 def is_full(board):
     return '-' not in sum(board,[])
 
-def is_winner(board,marker):
-    lines = [
+def possible_lines():
+    return [
         # rows
         (0,0, 0,1, 0,2),
         (1,0, 1,1, 1,2),
@@ -45,20 +50,46 @@ def is_winner(board,marker):
         (0,0, 1,1, 2,2),
         (0,2, 1,1, 2,0)  
         ]
-    for r1,c1, r2,c2, r3,c3 in lines:
+
+def is_winner(board,marker):
+    for r1,c1, r2,c2, r3,c3 in possible_lines():
         if board[r1][c1] + board[r2][c2] + board[r3][c3] == marker * 3: return True
     return False
 
+def eval(board,marker):
+    if marker == 'X':
+        opponent = 'O'
+    else:
+        opponent = 'X'
+    value = 0
+    for r1,c1, r2,c2, r3,c3 in possible_lines():
+        line = board[r1][c1] + board[r2][c2] + board[r3][c3]
+        if (marker in line and opponent in line) or line == '---':
+            # Line value = 0
+            pass
+        else:
+            if marker in line:
+                sign = 1
+            else:
+                sign = -1
+            value += sign * 10 ** (2-line.count('-'))
+    return value
 
-if __name__ == ’__main__’:
+
+if __name__ == '__main__':
     b = new_board()
     print(get(b,1,1))
     print(is_empty(b,1,1))
     print_board(b)
+    print()
 
     place(b,'X',1,1)
-    place(b,'X',0,0)
+    place(b,'O',1,0)
     place(b,'X',2,2)
+    place(b,'O',2,0)
+    
     print_board(b)
     print(is_full(b))
     print(is_winner(b,"X"))
+
+    print(eval(b,'O'))
